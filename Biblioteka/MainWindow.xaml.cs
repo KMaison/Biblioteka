69b0bcdb11599a07108664e39b6248c8fc6884b9
Karolina <karolina.maison1@gmail.com>
2019-05-09 15:26:44 +0200

    dodano wyswietlanie tabeli

--- Biblioteka/MainWindow.xaml.cs
+++ Biblioteka/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
 using System.Data;
+using System.Collections.ObjectModel;
 
 namespace Biblioteka
 {
@@ -38,6 +39,9 @@ namespace Biblioteka
 
     public partial class MainWindow : Window
     {
+        bool zalogowany = false;
+        int rola;
+        SqlConnection polaczenie;
         public MainWindow()
         {
             InitializeComponent();
@@ -45,7 +49,7 @@ namespace Biblioteka
             {
                 List<int> Roles = new List<int>();
 
-                SqlConnection polaczenie = new SqlConnection(@"Data source=LAPTOP-98O1O16J;
+                polaczenie = new SqlConnection(@"Data source=LAPTOP-98O1O16J;
                                                              database=Biblioteka;
                                                             Integrated Security = True");
                 polaczenie.Open();
@@ -59,19 +63,131 @@ namespace Biblioteka
                     Roles.Add(Int32.Parse(czytnik["Id"].ToString()));
                 }
                 czytnik.Close();
-                polaczenie.Close();
-
-                Console.ReadKey();
+               
             }
 
             catch (SqlException e)
             {
                 Console.WriteLine("Wystąpił nieoczekiwany błąd!");
                 Console.WriteLine(e.Message);
-                //  Console.ReadKey();
             }
 
 
+
+        }
+
+        ~MainWindow()
+        {
+            polaczenie.Close();
+        }
+        private void Login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            doButton.IsEnabled = false;
+            kolumna.IsEnabled = false;
+            tabele.IsEnabled = false;
+            switch (login.Text)
+            {
+                case "login":
+                    if(password.Password =="123")
+                    {
+                        zalogowany = true;
+                        roles.Items.Add(1);
+                        roles.Items.Add(3);
+                        roles.UpdateLayout();
+
+                        tabele.Items.Add("Ksiazka");
+                        
+                        kolumna.Items.Add("Tytul");
+                        kolumna.Items.Add("Rok_wydania");
+                        kolumna.Items.Add("Autor");
+                        kolumna.Items.Add("Id_ksiazki");
+
+                        akcja.Items.Add("Read");
+                        akcja.Items.Add("Update");
+                        akcja.Items.Add("Add");
+                        akcja.Items.Add("Delete");
+
+                        
+                        kolumna.UpdateLayout();
+                        tabele.UpdateLayout();
+                        akcja.UpdateLayout();
+
+                        login.IsEnabled = false;
+                        password.IsEnabled = false;
+                        button1.IsEnabled = false;
+                    }
+                    break;
+                   
+            }
+            
+        }
+
+
+        private void _do_Click(object sender, RoutedEventArgs e)
+        {
+            SqlCommand biblioteka = polaczenie.CreateCommand();
+            biblioteka.CommandText = $"SELECT * FROM [{(string)tabele.SelectedItem}]";
+            SqlDataReader czytnik = biblioteka.ExecuteReader();
+
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Tytul");
+            dt.Columns.Add("Rok_wydania");
+            dt.Columns.Add("Autor");
+            var colId = dt.Columns.Add("Id_ksiazki");
+            colId.ReadOnly = true;
+
+
+
+
+            while (czytnik.Read())
+            {
+                dt.Rows.Add(czytnik["Tytul"].ToString(),
+                    czytnik["Rok_wydania"].ToString(),
+                    czytnik["Autor"].ToString(),
+                    czytnik["Id_ksiazki"].ToString());
+            }
+
+            dataTable.ItemsSource = dt.AsDataView();
+            //dataTable.DataContext = dt.DefaultView;
+
+            czytnik.Close();
+        }
+
+        private void Kolumna_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            doButton.IsEnabled = true;
+        }
+
+        private void Akcja_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tabele.IsEnabled = true;
+        }
+
+        private void Roles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            rola = (int)roles.SelectedItem;
+            roles.IsEnabled = false;
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Tabele_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            kolumna.IsEnabled = true;
+        }
+
+        private void DataTable_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
         }
     }
